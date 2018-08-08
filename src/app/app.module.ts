ec365aba5ac968defe02bc108a7e9705c3d4195d
General_toytoy <lorre.ekul@gmail.com>
2018-08-08 12:16:43 +0800

    add new repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { EventsAppComponent } from './events-app.component';
+
+@NgModule({
+  declarations: [
+    EventsAppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
---
+++ src/app/events-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'events-app',
+  template: `
+  <img src="/assets/images/basic-shield.png" />
+  `
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
