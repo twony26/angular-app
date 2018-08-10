25bab1f6ea459a0e0c84665884530915cb301eca
General_toytoy <lorre.ekul@gmail.com>
2018-08-10 11:24:03 +0800

    add reusable services for toastr

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  // tslint:disable-next-line:component-selector
   selector: 'events-app',
   template: `
-  <nav-bar></nav-bar>
-  <events-list></events-list>
+    <nav-bar></nav-bar>
+    <events-list></events-list>
   `
 })
 export class EventsAppComponent {
-  title = 'ng-fundamentals';
+  title = 'app';
 }
